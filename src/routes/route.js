501faa065c0dfebcd86544b295e1b12d1d0640e9
Namdev Puri <namdevpuri3@gmail.com>
2022-04-14 07:23:18 +0530

    mongo2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const BookModel= require("../models/bookModel.js")
 const BookController= require("../controllers/bookController")
+// const UserModel= require("../models/userModel.js")
+// const UserController= require("../controllers/userController")
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/getUsersData", UserController.getUsersData)
+
+// router.post("/createBook", BookController.createBook  )
+
+// router.get("/getBooksData", BookController.getBooksData)
 
 router.post("/createBook", BookController.createBook  )
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/bookList", BookController.bookList)
+
+router.get("/getBooksInYear", BookController.getBooksInYear)
+
+router.get("/getRandomBooks", BookController.getRandomBooks)
+
+router.get("/getXINRBooks", BookController.getXINRBooks)
+
+router.get("/getParticularBooks", BookController.getParticularBooks)
 
 module.exports = router;
